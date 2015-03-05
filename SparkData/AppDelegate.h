6438861c9f75ce0e0558b25c3d669f9d1565ba43
Shayne Stark <shaynestark@live.com>
2015-03-05 12:33:16 -0500

    Initial Commit

---
+++ SparkData/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SparkData
+//
+//  Created by Jude Murphy on 3/5/15.
+//  Copyright (c) 2015 Spark Apps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SparkData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SparkData
+//
+//  Created by Jude Murphy on 3/5/15.
+//  Copyright (c) 2015 Spark Apps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
